d2e6203579b49a8b3766b19d7d6597f42eff73ea
Executive_Eagle <christophershelley257@gmail.com>
2026-09-20 20:41:39 -0500

    Align Staff modules with improved permission model

--- src/staff-system-v5.js
+++ src/staff-system-v5.js
@@ -1,5 +1,5 @@
 import { firebaseState, readDoc } from "./firebase.js";
-import { PERMISSIONS, hasPermission, isActiveStaff } from "./config/permissions.js?v=discord-role-sync-1";
+import { PERMISSIONS, hasPermission, isActiveStaff } from "./config/permissions.js?v=access-model-2";
 import { safe, lower, route } from "./utils.js";
 
 const MAIN_URL = "https://cognitus-solutions.org/";
