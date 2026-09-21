d4732158e41cf1e638daf33245a0cfd783b1f911
Executive_Eagle <christophershelley257@gmail.com>
2026-09-20 20:17:25 -0500

    Bust cache for trusted leave review workflow

--- src/extensions-loader-v4.js
+++ src/extensions-loader-v4.js
@@ -31,7 +31,7 @@ async function loadForRoute() {
   try {
     if (matches(current, G2_ROUTES)) {
       loadingSurface("staff workspace");
-      g2Promise ||= import("./generation2.js?v=20260920-v6-owner-leave-review");
+      g2Promise ||= import("./generation2.js?v=20260920-v7-trusted-leave-review");
       await g2Promise;
       return;
     }
